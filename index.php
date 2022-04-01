a60f5832c7c9d341f81da2e32f989d5b44667c0c
Hakan ALP <bm.hakanalp@gmail.com>
2022-04-01 15:31:53 +0300

    Prod added.

--- index.php
+++ index.php
@@ -60,9 +60,74 @@
 
                 <p></p>
                 <p></p>
+ <h2 class="my-4">UI Applications</h2>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th> 
+                            <th scope="col">Version</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //echo json_encode($json);
+                        $arr = [];
+                        $arr["Selfcare UI DEV"] =
+                            "https://selfcare-ui.dev.energia.gitops.vodafone.com/assets/version.json";
+                        $arr["Selfcare UI PPRD"] =
+                        "https://selfcare-ui.pprd.pre-prod-energia.vodafone.es/assets/version.json";
+                        $arr["Backoffice DEV"] =
+                        "https://backoffice-ui.dev.energia.gitops.vodafone.com/assets/version.json";
+                        $arr["Backoffice PPRD"] =
+                        "https://backoffice-ui.pprd.pre-prod-energia.vodafone.es/assets/version.json";
+                        $url = "";
+                        $i = 0;
+                        foreach ($arr as $key => $value) {
+                            $url = $value;
 
-               <!--   <h2 class="my-4">SI Microservices</h2>
-                <h3 class="my-4">DEV</h3>-->
+                            $json = @file_get_contents($url);
+                            if ($json === false) { ?>
+
+                                <tr class="table-danger">
+                                    <th scope="row"><?php echo $i; ?></th>
+                                    <td><a href="<?php echo $value; ?>"> <?php echo $key; ?> </a> </td>
+                                    <td></td>
+                                </tr>
+                            <?php } else {
+                                $json = @json_decode($json); 
+                                
+                                ?>
+                                 <?php
+                                 if($i%2==0){
+                                    $versions = explode("-",$json->version );
+                                }else{
+                                    $versions2 = explode("-",$json->version ); 
+                                } 
+                                    if(($i%2==1 && $i!=0) && ($versions[0] != $versions2[0]))	{
+                                        ?>   
+                                <tr class="table-warning">
+                                <?php
+                                  } else	{
+                                        ?>  
+                                         <tr class="table-success">
+                                        <?php
+                                   } 
+                                    
+                                    ?>
+                               
+                                    <th scope="row"><?php echo $i; ?></th>
+                                    <td><a href="<?php echo $value; ?>"> <?php echo $key; ?> </a> </td>
+                                    <td><?php echo $json->version; ?> </td>
+                                </tr>
+                        <?php }
+                            $i = $i + 1;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                
+                <h2 class="my-4">DEV</h2>-->
                 <table class="table table-bordered">
                     <thead>
                         <tr>
@@ -147,30 +212,30 @@
                     </thead>
                     <tbody>
                         <?php
-                        $arrP = [];
-                        $arrP["provision-api"] =
+                        $arrPP = [];
+                        $arrPP["provision-api"] =
                             "https://provision-api.pprd.pre-prod-energia.vodafone.es/api/provision/actuator/info";
-                        $arrP["shopping cart checkout"] =
+                        $arrPP["shopping cart checkout"] =
                             "https://shoppingcart-checkout-api.pprd.pre-prod-energia.vodafone.es/api/shoppingcart-checkout/actuator/info";
-                        $arrP["si-authorization"] =
+                        $arrPP["si-authorization"] =
                             "https://si-authorization-api.pprd.pre-prod-energia.vodafone.es/api/si-authorization/actuator/info";
-                        $arrP["ui-backend"] =
+                        $arrPP["ui-backend"] =
                             "https://ui-backend-api.pprd.pre-prod-energia.vodafone.es/api/ui-backend/actuator/info";
-                        $arrP["si-user-token"] =
+                        $arrPP["si-user-token"] =
                             "https://si-user-token-api.pprd.pre-prod-energia.vodafone.es/api/si-user-token/actuator/info";
-                        $arrP["biztalk-tmf-api"] =
+                        $arrPP["biztalk-tmf-api"] =
                             "https://biztalk-tmf-api.pprd.pre-prod-energia.vodafone.es/api/biztalk-tmf-api/actuator/info";
-                        $arrP["order-create-service"] =
+                        $arrPP["order-create-service"] =
                             "https://order-create-service-api.pprd.pre-prod-energia.vodafone.es/api/order-create-service/actuator/info";
-                        $arrP["payment-methods-api"] =
+                        $arrPP["payment-methods-api"] =
                             "https://payment-methods-api.pprd.pre-prod-energia.vodafone.es/api/paymentMethods/v1/actuator/info";
-                        $arrP["si-bpmn"] =
+                        $arrPP["si-bpmn"] =
                             "https://si-bpmn-api.pprd.pre-prod-energia.vodafone.es/api/si-bpmn/actuator/info";
-                        $arrP["northbound"] =
+                        $arrPP["northbound"] =
                             "https://northbound-api.pprd.pre-prod-energia.vodafone.es/api/northbound/actuator/info";
                         $url = "";
                         $i = 1;
-                        foreach ($arrP as $key => $value) {
+                        foreach ($arrPP as $key => $value) {
                             $url = $value;
                             $urlD = $arrD[$key];
 
@@ -233,51 +298,78 @@
                     </tbody>
                 </table>
                 
- <h2 class="my-4">UI Applications</h2>
+<h3 class="my-4">PPOD</h3>
                 <table class="table table-bordered">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">Name</th> 
-                            <th scope="col">Version</th> 
+                            <th scope="col">Name</th>
+							<th scope="col">Version</th>
+                            <th scope="col">Commit-id</th>
+                            <th scope="col">Commit-time</th>
+                            <th scope="col">Branch</th> 
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                        //echo json_encode($json);
-                        $arr = [];
-                        $arr["Selfcare UI DEV"] =
-                            "https://selfcare-ui.dev.energia.gitops.vodafone.com/assets/version.json";
-                        $arr["Selfcare UI PPRD"] =
-                        "https://selfcare-ui.pprd.pre-prod-energia.vodafone.es/assets/version.json";
-                        $arr["Backoffice DEV"] =
-                        "https://backoffice-ui.dev.energia.gitops.vodafone.com/assets/version.json";
-                        $arr["Backoffice PPRD"] =
-                        "https://backoffice-ui.pprd.pre-prod-energia.vodafone.es/assets/version.json";
+                        $arrP = [];
+                        $arrP["provision-api"] =
+                            "https://provision-api.energia.vodafone.es/api/provision/actuator/info";
+                        $arrP["shopping cart checkout"] =
+                            "https://shoppingcart-checkout-api.energia.vodafone.es/api/shoppingcart-checkout/actuator/info";
+                        $arrP["si-authorization"] =
+                            "https://si-authorization-api.energia.vodafone.es/api/si-authorization/actuator/info";
+                        $arrP["ui-backend"] =
+                            "https://ui-backend-api.energia.vodafone.es/api/ui-backend/actuator/info";
+                        $arrP["si-user-token"] =
+                            "https://si-user-token-api.energia.vodafone.es/api/si-user-token/actuator/info";
+                        $arrP["biztalk-tmf-api"] =
+                            "https://biztalk-tmf-api.energia.vodafone.es/api/biztalk-tmf-api/actuator/info";
+                        $arrP["order-create-service"] =
+                            "https://order-create-service-api.energia.vodafone.es/api/order-create-service/actuator/info";
+                        $arrP["payment-methods-api"] =
+                            "https://payment-methods-api.energia.vodafone.es/api/paymentMethods/v1/actuator/info";
+                        $arrP["si-bpmn"] =
+                            "https://si-bpmn-api.energia.vodafone.es/api/si-bpmn/actuator/info";
+                        $arrP["northbound"] =
+                            "https://northbound-api.energia.vodafone.es/api/northbound/actuator/info";
                         $url = "";
-                        $i = 0;
-                        foreach ($arr as $key => $value) {
+                        $i = 1;
+                        foreach ($arrP as $key => $value) {
                             $url = $value;
+                            $urlD = $arrPP[$key];
 
                             $json = @file_get_contents($url);
+                            $jsonD = @file_get_contents($urlD);
                             if ($json === false) { ?>
-
+ 
                                 <tr class="table-danger">
                                     <th scope="row"><?php echo $i; ?></th>
                                     <td><a href="<?php echo $value; ?>"> <?php echo $key; ?> </a> </td>
-                                    <td></td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
                                 </tr>
-                            <?php } else {
+                            <?php } else if ($jsonD===false) { ?> 
+                                <tr class="table-warning">
+                                    <th scope="row"><?php echo $i; ?></th>
+                                    <td><a href="<?php echo $value; ?>"> <?php echo $key; ?> </a> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                </tr>
+                            <?php
+
+                            } else {//echo json_encode($json);
                                 $json = @json_decode($json); 
-                                
+                                $jsonD = @json_decode($jsonD);
                                 ?>
                                  <?php
-                                 if($i%2==0){
-                                    $versions = explode("-",$json->version );
-                                }else{
-                                    $versions2 = explode("-",$json->version ); 
-                                } 
-                                    if(($i%2==1 && $i!=0) && ($versions[0] != $versions2[0]))	{
+                                    $versions = explode("-",$json->build->version );
+                                    $versions2 = explode("-",$jsonD->build->version );
+                                    if($versions[0] != $versions2[0])	{
                                         ?>   
                                 <tr class="table-warning">
                                 <?php
@@ -288,10 +380,16 @@
                                    } 
                                     
                                     ?>
-                               
                                     <th scope="row"><?php echo $i; ?></th>
                                     <td><a href="<?php echo $value; ?>"> <?php echo $key; ?> </a> </td>
-                                    <td><?php echo $json->version; ?> </td>
+                                   
+                                         <td> <?php echo $json->build->version; ?> </td>
+                                        <td><?php echo $json->git->commit
+                                            ->id; ?> </td>
+                                        <td><?php echo $json->git->commit
+                                            ->time; ?> </td>
+                                        <td><?php echo $json->git
+                                            ->branch; ?> </td> 
                                 </tr>
                         <?php }
                             $i = $i + 1;
@@ -299,6 +397,8 @@
                         ?>
                     </tbody>
                 </table>
+				
+				
 <h2 class="my-4">R&D Microservices DEV</h2>
                 <table class="table table-bordered">
                     <thead>
@@ -453,7 +553,71 @@
                         ?>
                     </tbody>
                 </table>
+<h2 class="my-4">R&D Microservices PROD</h2>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //echo json_encode($json);
+						$arr = array();
+						$arr['Account Management'] = 'https://dacm-api.energia.gitops.vodafone.com/api/accountManagement/v4/actuator/info';
+						$arr['aggregate-api'] =  'https://aggregate-api.energia.gitops.vodafone.com/api/aggregate/v1/actuator/info';
+						$arr['Agreement Management'] = 'https://dagm-api.energia.gitops.vodafone.com/api/agreementManagement/v4/actuator/info';
+						$arr['backoffice-api'] = 'https://backoffice-api.energia.gitops.vodafone.com/api/backofficeTaskManagement/v4/actuator/info';
+						$arr['Customer Management'] = 'https://dcm-api.energia.gitops.vodafone.com/api/customerManagement/v4/actuator/info';
+						$arr['Document Management'] = 'https://ddms-api.energia.gitops.vodafone.com/api/documentManagement/v4/actuator/info';
+						$arr['Geographic Address  Management'] = 'https://dgeo-api.energia.gitops.vodafone.com/api/geographicAddressManagement/v4/actuator/info';
+						$arr['Partnership Management'] = 'https://dpm-api.energia.gitops.vodafone.com/api/partnershipManagement/v4/actuator/info';
+						$arr['Party Management'] = 'https://dpam-api.energia.gitops.vodafone.com/api/partyManagement/v4/actuator/info';
+						$arr['Party Role Management'] = 'https://dpr-api.energia.gitops.vodafone.com/api/partyRoleManagement/v4/actuator/info';
+						$arr['Product Catalog'] = 'https://dpc-api.energia.gitops.vodafone.com/api/productCatalogManagement/v4/actuator/info';
+						$arr['Product Inventory Management'] = 'https://dpim-api.energia.gitops.vodafone.com/api/productInventoryManagement/v4/actuator/info'; 
+						$arr['Product Ordering Management'] = 'https://dpom-api.energia.gitops.vodafone.com/api/productOrderingManagement/v4/actuator/info';
+						$arr['Reference Management'] = 'https://reference-management-api.energia.gitops.vodafone.com/api/referenceManagement/v4/actuator/info';
+						$arr['Resource Inventory Management'] = 'https://dri-api.energia.gitops.vodafone.com/api/resourceInventoryManagement/v4/actuator/info'; 
+						$arr['Resource Catalog Management'] = 'https://drc-api.energia.gitops.vodafone.com/api/resourceCatalogManagement/v4/actuator/info';
+						$arr['Resource Ordering Management'] = 'https://dro-api.energia.gitops.vodafone.com/api/resourceOrderingManagement/v4/actuator/info';
+						$arr['Resource Order Fulfillment'] = 'https://drof-api.energia.gitops.vodafone.com/api/resourceOrderFulfillment/v4/actuator/info';
+						$arr['S3'] = 'https://s3-api.energia.gitops.vodafone.com/api/storageService/v4/actuator/info';
+						$arr['Service Catalog Management'] = 'https://dsc-api.energia.gitops.vodafone.com/api/serviceCatalogManagement/v4/actuator/info';
+						$arr['Service Ordering Management'] = 'https://dso-api.energia.gitops.vodafone.com/api/serviceOrderingManagement/v4/actuator/info';
+						$arr['Service Inventory Management'] = 'https://dsi-api.energia.gitops.vodafone.com/api/serviceInventoryManagement/v4/actuator/info';
+						$arr['Service Ordering Fulfillment'] = 'https://dsof-api.energia.gitops.vodafone.com/api/serviceOrderFulfillment/v4/actuator/info';
 
+                        $url = "";
+                        $i = 1;
+                        foreach ($arr as $key => $value) {
+                            $url = $value;
+
+                            $json = @file_get_contents($url);
+                            if ($json === FALSE) {
+                        ?>
+
+                                <tr class="table-danger">
+                                    <th scope="row"><?php echo $i ?></th>
+                                    <td><a href="<?php echo $value ?>"> <?php echo $key ?> </a> </td>
+                                </tr>
+                            <?php
+                            } else {
+                                $json = @json_decode($json);
+
+                            ?>
+                                <tr class="table-success">
+                                    <th scope="row"><?php echo $i ?></th>
+                                    <td><a href="<?php echo $value ?>"> <?php echo $key ?> </a> </td>
+                                </tr>
+                        <?php
+                            }
+                            $i = $i + 1;
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
             <!-- /.col-lg-9 -->
 
